526c8a3de2a8b1120570d36971f38f1bf8569c5c
braumo <braumo@gasos-ro.c>
2025-02-17 09:14:43 +0100

    Hotovo

--- src/Ddodavatel.java
+++ src/Ddodavatel.java
@@ -10,15 +10,13 @@ public class Ddodavatel extends Delnik{
     @Override
     public void run() {
         while (getSoucatsky().maximalnipocetpanenek()) {
-
-            if (getMaterial().getPlast() < 220){
                 System.out.println("Začal dodávat plast pro panenky");
                 getMaterial().pridatplast();
-            }
-            if (getMaterial().getVlasy() < 4){
+
+
                 System.out.println("Začal dodávat vlasy pro panenku");
                 getMaterial().pridavlasy();
-            }
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
